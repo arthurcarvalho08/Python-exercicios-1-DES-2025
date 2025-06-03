d265e63a73cc867055346ae26efc27c7a2b0deff
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-06-03 13:36:12 +0000

    exercicio03.py

--- exercicio03.py
+++ exercicio03.py
@@ -1,3 +1,10 @@
 # Rafael trabalha com armazenamento de grãos e precisa garantir que a umidade do ar no local não ultrapasse 70%.
 # Escreva um programa que receba o valor da umidade atual e exiba um alerta se estiver acima do limite.
 
+umidade = 70 
+
+if umidade > 70 :
+    print("Umidade acima do permitido.")
+
+elif umidade < 70 :
+    print("Umidade acima do permitido.")
