c7a087bf5d0945ffb0385d7aafab638319667c32
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-06-03 14:11:16 +0000

    exercicio06.py nao fin.

--- exercicio06.py
+++ exercicio06.py
@@ -1,3 +1,12 @@
 # Bianca está programando o controle de acesso a uma plataforma que só funciona entre 9h e 21h.
 # O programa deve receber a hora atual (formato 24h) e informar se o acesso é permitido.
 
+hora = int(input("Digite a hora atual."))
+
+if hora >= 9 < 21 :
+    print("A plataforma pode ser acessada.")
+else:
+    print("A plataforma não pode ser acessada.")
+
+    #não finalizado.
+
