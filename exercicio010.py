90459967109e990cecb8df5b7ef06cb61e891667
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-06-05 14:32:41 +0000

    finalizado 10

--- exercicio010.py
+++ exercicio010.py
@@ -1,7 +1,11 @@
-de R$ 3.000,00
+#de R$ 3.000,00
 # A parcela não pode ser maior que 35% do salário
 
 solicitar_financiamento = int(input("Envie seu salário mensal:"))
 
 if solicitar_financiamento == 3000:
-    print("")
+    print("Salário dentro do financiamento.")
+else:
+    print("Salário fora do financiamento.")
+
+#finalizado
