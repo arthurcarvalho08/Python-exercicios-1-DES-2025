90fd3aa07b20a1f2c8667f5ede5673591c2ea84a
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-06-05 12:17:26 +0000

    finalizado 9

--- exercicio09.py
+++ exercicio09.py
@@ -1,6 +1,9 @@
 # Douglas está criando uma função simples no jogo que verifica se o número é múltiplo de 2 ou não (par ou ímpar).
 # Escreva um programa que faça essa verificação.
+numero = int(input("Digite o numero ="))
+if numero % 2 == 0 :
+    print("Par.")
+else:
+    print("Ímpar.")
 
-numero = int(input("Digite o número."))
-
-if numero = 2, 4, 6, 8, 10 
+#finalizado
