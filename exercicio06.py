256dbe32b49ec26badcf5649b8e55a4a61cefa7d
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-06-05 12:20:20 +0000

    finalizado 6

--- exercicio06.py
+++ exercicio06.py
@@ -1,11 +1,4 @@
 # Bianca está programando o controle de acesso a uma plataforma que só funciona entre 9h e 21h.
 # O programa deve receber a hora atual (formato 24h) e informar se o acesso é permitido.
 
-hora = int(input("Digite a hora atual."))
 
-if hora >= 9 :
-    print("Acesso permitido.")
-elif hora > 21 :
-    print("Acesso negado.")
-else:
-    print("Acesso negado.")
