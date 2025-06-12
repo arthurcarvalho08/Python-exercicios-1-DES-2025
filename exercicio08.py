c891f86f7c6e101193306067643411efe0999958
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-06-12 12:05:34 +0000

    exercicio 8 concluido

--- exercicio08.py
+++ exercicio08.py
@@ -4,7 +4,12 @@
 # De 51 a 150 km: R$ 15,00
 # Acima de 150 km: R$ 25,00
 
-distancia = int(input("Digite a distância em km."))
+distancia = int(input("Digite a distancia em KM"))
 
-if distancia == 50 :
-    print("O preço ")
+if distancia <= 50 :
+    print("O preço do frete seria a 5 reais.")
+elif distancia <= 150 : 
+    print("o prço do frete será 15 reais.")
+else:
+    print("O preço do frete será 25 reais.")
+#finalizado
