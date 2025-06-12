911340bab68abea033369d67c4b5bcbb2d16ee4a
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-06-12 14:26:23 +0000

    exercicio 11 alterar

--- exercicio011.py
+++ exercicio011.py
@@ -4,3 +4,17 @@
 #Peso normal (18.5 a 24.9)
 #Sobrepeso (25 a 29.9)
 #Obesidade (>= 30)
+peso = float(input("Digite seu peso em kg."))
+altura = float(input("Digite sua altura em metros."))
+imc = peso / (altura ** 2)
+
+if peso < 18.5:
+    print("Abaixo do peso.")
+elif peso >= 18.5 <= 24.9:
+    print("Peso normal.")
+elif peso >= 25 <= 29.9:
+    print("Sobrepeso.")
+else:
+    print("Obesidade.")
+
+#alterar
