d804fba7b2f5873b4ab68f1a761e59817c72a93e
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-06-26 12:18:34 +0000

    ATIVIDADES

---
+++ texto.py
@@ -0,0 +1,8 @@
+print("Olá, mundo!")
+      
+nome = "Arthur" 
+sobrenome = "Carvalho"
+print("Nome completo: " + nome +" " + sobrenome)
+
+idade = 15
+print(f"{nome} {sobrenome} tem {idade} anos.")
