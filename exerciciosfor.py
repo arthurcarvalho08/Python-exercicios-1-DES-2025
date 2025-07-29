cbc58d910e417e1e2c161b214a3b9cb3518d63d9
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2025-07-29 14:10:45 +0000

    exercicio 1 finalizado

---
+++ exerciciosfor.py
@@ -0,0 +1,14 @@
+#Imprima os números pares de 0 a 10.
+#Conte quantas vogais existem na palavra “programação”.
+#Calcule o fatorial de 5 (5! = 5 × 4 × 3 × 2 × 1).
+#Crie uma lista de frutas (ex: maçã, banana, morango) e imprima cada uma
+
+for i in range (11):
+    if i % 2 == 0:
+        print(i)
+#
+#
+#
+for i in range ()
+
+
